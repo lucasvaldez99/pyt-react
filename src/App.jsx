ad5938d887ca6b43ec76bd467c9987c9b39fdf40
jorge valdez <lucasvaldez99@gmail.com>
2022-08-29 21:21:46 -0300

    contador

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,25 @@ import Nav from './componentes/nav/nav'
 import Main from './componentes/main/main';
 import Footer from './componentes/footer/footer';
 import ItemListContainer from './componentes/itemListContainer/itemListContainer';
+import Contador from './componentes/contador/contador';
+
+
 
 const App = () => {
 
   const bienvenida = 'bienvenidos a nuestra pagina web'
+ 
+  let stock = 15
+  let initial = 0
+ 
+const onAdd = ()=>{
+  alert("agregaste productos al carrito")
+}
   return (
     <>
     <Nav/>
     <ItemListContainer bienvenida={bienvenida}/>
+    <Contador stock={stock} initial={initial} onAdd={onAdd}/>
     <Main/>
     <Footer/>
     </>
---
+++ src/componentes/contador/contador.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+import estilos from './contador.module.css'
+
+const Contador = ({stock,initial,onAdd}) =>{
+ 
+   const [count, setCount] = useState(0)
+ 
+ 
+const sumar = () =>{
+   count < stock ? setCount(count + 1) : alert('llegaste al maximo que podes comprar')
+}
+
+const restar = ()=>{
+   count > initial ? setCount(count - 1) : alert("tenes que agregar productos")
+}
+
+
+
+
+    return(
+        <div className={estilos.contenedorDiv}>
+            <button className={estilos.acciones} onClick={sumar}>+</button>
+            <button className={estilos.acciones} onClick={restar}>-</button>
+            <p className={estilos.Productos}>Productos = {count}</p>
+            <button className={estilos.btn} onClick={onAdd}> agregar al carrito</button>
+        </div>
+    )
+}
+
+export default Contador
