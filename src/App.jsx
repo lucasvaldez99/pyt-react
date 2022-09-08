d68c31f3026071561fcee9da143396c0cfa1000f
jorge valdez <lucasvaldez99@gmail.com>
2022-09-07 23:04:35 -0300

    correccion

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import './App.css';
 import Nav from './componentes/nav/nav'
-import Main from './componentes/main/main';
 import Footer from './componentes/footer/footer';
 import ItemListContainer from './componentes/itemListContainer/itemListContainer';
 import Contador from './componentes/contador/contador';
 import ItemDetailContainerm from './componentes/itemDetailContainer/itemDetailContainer';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
 
 
 const App = () => {
@@ -19,16 +19,26 @@ const onAdd = ()=>{
   alert("agregaste productos al carrito")
 }
   return (
-    <>
-    <Nav/>
-    <Main/>
-    <ItemListContainer bienvenida={bienvenida}/>
-    <ItemDetailContainerm/>
-    <Contador stock={stock} initial={initial} onAdd={onAdd}/>
    
+    <BrowserRouter>
+    <Nav/>
+    
+
+     <Routes>
+     
+      <Route path="/" element={ <ItemListContainer bienvenida={bienvenida}/>}/>
+      
+      <Route path='/itemDetail/:idProd' element={<ItemDetailContainerm/>}/>
+   
+       <Route path='/categorias/:categoriasName'element={<ItemListContainer/>}/>
+
+      <Route path='/contador'  element={<Contador stock={stock} initial={initial} onAdd={onAdd}/>}    /> 
+    </Routes>
+
+
     <Footer/>
     
-    </>
+    </BrowserRouter>
   )
 }
 
